05bc89030130d628c4bad333d82ad851db4f51f3
Nihar Sheth <67347075+NiharSheth65@users.noreply.github.com>
2023-02-23 21:51:51 -0500

    Add files via upload

--- src/main/java/frc/robot/variables.java
+++ src/main/java/frc/robot/variables.java
@@ -5,7 +5,9 @@ public class variables {
     int leftBackMotorID = 1;
     int rightFrontMotorID = 4;
     int rightBackMotorID = 3;  
-    int jawMotorID = 5;  
+    int jawLowerMotorID = 5;  
+    int jawUpperMotorID = 6; 
+    int lowerIntakeMortorID = 7;
 
     // joy stick axises
     int joyStickLeft_AxisY = 1; 
@@ -14,6 +16,30 @@ public class variables {
     // button channels 
     int buttonAChannel = 1; 
     int buttonBChannel = 2; 
+    int buttonYChannel = 4;
     int buttonRBChannel = 5; 
     int buttonLBChannel = 6; 
+    int buttonLeftChannel = 9;
+    int buttonRightChannel = 10;
+
+    // safty stuff 
+    double safetyFactor = 0.5; 
+    double turnRate = 0.5; 
+
+    // pid stuff 
+    double distanceKP = 0.05;
+    double gyroKP = 0.025;
+
+    // piston timings 
+    double pistonQuick = 50; 
+    double pistonSlow = 100; 
+
+    // lower jaws pid values 
+    double lower_jaw_kp = 0.7; 
+    double lower_jaw_ki = 0.05; 
+    double lower_jaw_kd = 0; 
+
+
+
+ 
 }
